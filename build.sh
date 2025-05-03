a94e65b2282b8c0fa4e1907be70b42d16aebfed1
oItsMineZ <135024283+oItsMineZ@users.noreply.github.com>
2025-05-02 20:06:33 -0700

    build: introduce build script

---
+++ build.sh
@@ -0,0 +1,397 @@
+#!/bin/bash
+
+abort ()
+{
+    cd -
+    echo "---------------------------------------------------------"
+    echo "-- Kernel Compilation Failed! Exiting..."
+    echo "---------------------------------------------------------"
+
+    if [[ "$LOCAL" == "y" ]]; then
+        clean
+    fi
+
+    exit -1
+}
+
+unset_flags ()
+{
+    cat << EOF
+Usage: $(basename "$0") [options]
+Options:
+    -m, --model [value]    Specify the Model Code of the Phone (default: d2s)
+    -k, --ksu [y/N]        Include KernelSU Next with SuSFS (default: y)
+    -h, --help             List all Build Script Command
+    -c, --clean [y/N]      Reset all Change to Latest Commit [!! Your Uncommit Change will Lost !!] (default: n)
+EOF
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --model|-m)
+            MODEL="$2"
+            shift 2
+            ;;
+        --ksu|-k)
+            KSU_OPTION="$2"
+            shift 2
+            ;;
+        --ver|-v)
+            KERNEL_VERSION="$2"
+            shift 2
+            ;;
+        --rel|-r)
+            RELEASE="$2"
+            shift 2
+            ;;
+        --help|-h)
+            unset_flags
+            exit 1
+            ;;
+        --clean|-c)
+            CLEAN="$2"
+            shift 2
+            ;;
+        *)\
+            unset_flags
+            exit 1
+            ;;
+    esac
+done
+
+detect_env ()
+{
+    DATE=`date +"%Y%m%d"`
+    export KBUILD_BUILD_USER=oItsMineZ
+    export KBUILD_BUILD_HOST=Stable
+
+    echo "---------------------------------------------------------"
+
+    if [ ! -z $RELEASE ]; then
+        echo "-- Running on GitHub Actions..."
+    else
+        echo "-- Running on Local Machine..."
+        LOCAL=y
+    fi
+
+    # Set Build Variable
+    if [ -z $KERNEL_VERSION ]; then
+        KERNEL_VERSION=Unofficial
+    fi
+
+    if [ -z $KSU ]; then
+        KSU=y
+    fi
+
+    if [ -z $CLEAN ]; then
+        CLEAN=n
+    fi
+}
+
+DIR=$(pwd)
+
+if [ -z $MODEL ]; then
+    MODEL=d2s
+fi
+
+KERNEL_DEFCONFIG=oitsminez-"$MODEL"_defconfig
+case $MODEL in
+beyond0lte)
+    SOC=9820
+    BOARD=SRPRI28A014KU
+;;
+beyond1lte)
+    SOC=9820
+    BOARD=SRPRI28B014KU
+;;
+beyond2lte)
+    SOC=9820
+    BOARD=SRPRI17C014KU
+;;
+beyondx)
+    SOC=9820
+    BOARD=SRPSC04B011KU
+;;
+d1)
+    SOC=9825
+    BOARD=SRPSD26B007KU
+;;
+d1xks)
+    SOC=9825
+    BOARD=SRPSD23A002KU
+;;
+d2s)
+    SOC=9825
+    BOARD=SRPSC14B007KU
+;;
+d2x)
+    SOC=9825
+    BOARD=SRPSC14C007KU
+;;
+*)
+    unset_flags
+    exit
+esac
+
+kernelsu ()
+{
+    KSU_NEXT=ksu_next.config
+
+    if test -d "$DIR/drivers/kernelsu" && grep -rnw 'fs/Makefile' -e 'CONFIG_KSU_SUSFS'; then
+        echo "---------------------------------------------------------"
+        echo "-- KernelSU-Next Directory Found!..."
+        echo "---------------------------------------------------------"
+    else
+        echo "---------------------------------------------------------"
+        echo "-- Checkout oItsMineZ's KernelSU-Next Repo..."
+        echo "---------------------------------------------------------"
+
+        git submodule add --force https://github.com/oItsMineZ/KernelSU-Next
+        curl -LSs "https://raw.githubusercontent.com/oItsMineZ/KernelSU-Next/susfs-v1.5.5/kernel/setup.sh" | bash -
+
+        if ! grep -rnw 'fs/Makefile' -e 'CONFIG_KSU_SUSFS'; then
+
+            echo "---------------------------------------------------------"
+            echo "-- Patch Kernel Tree with SuSFS..."
+            echo "---------------------------------------------------------"
+
+            curl -LOSs "https://raw.githubusercontent.com/oItsMineZKernel/Kernel-Patch/refs/heads/main/SuSFS.patch" && patch -p1 < SuSFS.patch && rm -rf *.patch
+        fi
+    fi
+}
+
+toolchain ()
+{
+    echo "---------------------------------------------------------"
+    echo "-- Checkout Toolchain Repo..."
+    echo "---------------------------------------------------------"
+
+    git submodule add --branch aarch64-linux-android-4.9 --force https://github.com/oItsMineZKernel/toolchain toolchain/aarch64-linux-android-4.9
+    git submodule add --branch clang-r416183b1 --force https://github.com/oItsMineZKernel/toolchain toolchain/clang-r416183b1
+
+    CLANG=$DIR/toolchain/clang-r416183b1/bin/
+    GCC=$DIR/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+    ARGS="
+        ARCH=arm64 O=out \
+        CC=${CLANG}clang \
+        CLANG_TRIPLE=${CLANG}aarch64-linux-gnu- \
+        CROSS_COMPILE=$GCC
+    "
+}
+
+kernel ()
+{
+    if [[ "$SOC" == "9825" ]]; then
+        DEVICE=Note10
+    else
+        DEVICE=S10
+    fi
+
+    # Build Kernel Image
+    echo "---------------------------------------------------------"
+    echo "-- Device: $DEVICE ("$MODEL")"
+    echo "-- SOC: Exynos$SOC"
+    echo "-- Defconfig: $KERNEL_DEFCONFIG"
+    echo "-- Kernel Version: $KERNEL_VERSION"
+    echo "-- Build Date: `date +"%Y-%m-%d"`"
+
+    if [ -z $KSU_NEXT ]; then
+        echo "-- KernelSU Next with SuSFS: Not Include"
+    else
+        echo "-- KernelSU Next with SuSFS: $KSU_NEXT"
+    fi
+
+    sed -i "s/CONFIG_LOCALVERSION=\"\"/CONFIG_LOCALVERSION=\"-oItsMineZKernel-$KERNEL_VERSION-$DEVICE-$MODEL\"/" $DIR/arch/arm64/configs/$KERNEL_DEFCONFIG
+    sed -i "s/CONFIG_LOCALVERSION_AUTO=\"y\"/CONFIG_LOCALVERSION_AUTO=\"n\"/" $DIR/arch/arm64/configs/$KERNEL_DEFCONFIG
+
+    if [ ! -z $RELEASE ]; then
+        echo BUILD_DEVICE=$DEVICE >> $GITHUB_ENV
+    fi
+
+    DEFCONFIG="$KERNEL_DEFCONFIG oitsminez.config $KSU_NEXT"
+
+    echo "---------------------------------------------------------"
+    echo "-- Building Kernel Using "$KERNEL_DEFCONFIG""
+    echo "-- Generating Configuration Files..."
+    echo "---------------------------------------------------------"
+
+    make -j$(nproc --all) $ARGS $DEFCONFIG || abort
+
+    echo "---------------------------------------------------------"
+    echo "-- Building Kernel..."
+    echo "---------------------------------------------------------"
+
+    make -j$(nproc --all) $ARGS || abort
+
+    echo "---------------------------------------------------------"
+    echo "-- Finished Kernel Build!"
+    echo "---------------------------------------------------------"
+
+    rm -rf $DIR/build/out/$MODEL
+    mkdir -p $DIR/build/out/$MODEL
+}
+
+dtb ()
+{
+    # Build DTB Image
+    echo "-- Building Device Tree Blob Image for exynos$SOC..."
+    echo "---------------------------------------------------------"
+
+    $DIR/build/mkdtimg cfg_create $DIR/build/out/$MODEL/dtb_exynos$SOC.img \
+        $DIR/build/dtconfigs/exynos$SOC.cfg \
+        -d $DIR/out/arch/arm64/boot/dts/exynos
+
+    # Build DTBO Image
+    echo "---------------------------------------------------------"
+    echo "-- Building Device Tree Blob Image for $DEVICE ($MODEL)..."
+    echo "---------------------------------------------------------"
+
+    $DIR/build/mkdtimg cfg_create $DIR/build/out/$MODEL/dtbo_$MODEL.img \
+        $DIR/build/dtconfigs/$MODEL.cfg \
+        -d $DIR/out/arch/arm64/boot/dts/samsung
+}
+
+ramdisk ()
+{
+    # Build Ramdisk
+    echo "---------------------------------------------------------"
+    echo "-- Building Ramdisk..."
+    echo "---------------------------------------------------------"
+
+    rm -rf $DIR/build/AIK/s*
+    mkdir -p $DIR/build/AIK/split_img
+    cp $DIR/out/arch/arm64/boot/Image $DIR/build/AIK/split_img/boot.img-kernel
+    echo -e "0x10000000" > build/AIK/split_img/boot.img-base
+    echo -e $BOARD > build/AIK/split_img/boot.img-board
+    echo -e "loop.max_part=7" > build/AIK/split_img/boot.img-cmdline
+    echo -e "sha1" > build/AIK/split_img/boot.img-hashtype
+    echo -e "1" > build/AIK/split_img/boot.img-header_version
+    echo -e "AOSP" > build/AIK/split_img/boot.img-imgtype
+    echo -e "0x00008000" > build/AIK/split_img/boot.img-kernel_offset
+    echo -e "45285376" > build/AIK/split_img/boot.img-origsize
+    echo -e "2023-04" > build/AIK/split_img/boot.img-os_patch_level
+    echo -e "12.0.0" > build/AIK/split_img/boot.img-os_version
+    echo -e "2048" > build/AIK/split_img/boot.img-pagesize
+    echo -e "0x01000000" > build/AIK/split_img/boot.img-ramdisk_offset
+    echo -e "gzip" > build/AIK/split_img/boot.img-ramdiskcomp
+    echo -e "0xf0000000" > build/AIK/split_img/boot.img-second_offset
+    echo -e "0x00000100" > build/AIK/split_img/boot.img-tags_offset
+
+    # Create Boot Image
+    echo "-- Creating Boot Image..."
+    echo "---------------------------------------------------------"
+
+    mkdir -p $DIR/build/AIK/ramdisk/debug_ramdisk
+    mkdir -p $DIR/build/AIK/ramdisk/dev
+    mkdir -p $DIR/build/AIK/ramdisk/mnt
+    mkdir -p $DIR/build/AIK/ramdisk/proc
+    mkdir -p $DIR/build/AIK/ramdisk/sys
+
+    rm -rf $DIR/build/AIK/ramdisk/f*
+
+    cp $DIR/build/AIK/fstab.exynos$SOC $DIR/build/AIK/ramdisk/
+
+    cd $DIR/build/AIK && ./repackimg.sh --nosudo
+}
+
+build_zip ()
+{
+    # Build Zip
+    echo "---------------------------------------------------------"
+    echo "-- Building Zip..."
+    if [[ "$LOCAL" == "y" ]] || [[ "$RELEASE" == "y" ]]; then
+        echo "---------------------------------------------------------"
+    fi
+
+    rm -rf $DIR/build/out/$MODEL/zip
+    mkdir -p $DIR/build/export
+    mkdir -p $DIR/build/out/$MODEL/zip
+    mkdir -p $DIR/build/out/$MODEL/zip/module
+    mkdir -p $DIR/build/out/$MODEL/zip/module/common/
+    mkdir -p $DIR/build/out/$MODEL/zip/module/META-INF/com/google/android
+    mkdir -p $DIR/build/out/$MODEL/zip/META-INF/com/google/android
+    mv $DIR/build/AIK/image-new.img $DIR/build/out/$MODEL/boot-patched.img
+
+    cp $DIR/build/out/$MODEL/boot-patched.img $DIR/build/out/$MODEL/zip/boot.img
+    cp $DIR/build/out/$MODEL/dtb_exynos$SOC.img $DIR/build/out/$MODEL/zip/dtb.img
+    cp $DIR/build/out/$MODEL/dtbo_$MODEL.img $DIR/build/out/$MODEL/zip/dtbo.img
+    cp $DIR/build/update-binary $DIR/build/out/$MODEL/zip/META-INF/com/google/android/
+    cp $DIR/build/updater-script $DIR/build/out/$MODEL/zip/META-INF/com/google/android/
+
+    cp $DIR/build/module.prop $DIR/build/out/$MODEL/zip/module/
+    cp $DIR/build/system.prop $DIR/build/out/$MODEL/zip/module/common/
+    cp $DIR/build/module-binary $DIR/build/out/$MODEL/zip/module/META-INF/com/google/android/update-binary
+    echo -e "#MAGISK" > $DIR/build/out/$MODEL/zip/module/META-INF/com/google/android/updater-script
+
+    cd $DIR/build/out/$MODEL/zip/module
+    zip -r ../module.zip .
+    rm -rf $DIR/build/out/$MODEL/zip/module
+
+    sed -i "s/ui_print(\" Kernel Version: \");/ui_print(\" Kernel Version: $KERNEL_VERSION\");/" $DIR/build/out/$MODEL/zip/META-INF/com/google/android/updater-script
+    sed -i "s/ui_print(\" Kernel Device: \");/ui_print(\" Kernel Device: $DEVICE ($MODEL)\");/" $DIR/build/out/$MODEL/zip/META-INF/com/google/android/updater-script
+    sed -i "s/CONFIG_LOCALVERSION=\"-oItsMineZKernel-$KERNEL_VERSION-"$DEVICE"-$MODEL\"/CONFIG_LOCALVERSION=\"-oItsMineZKernel-$KERNEL_VERSION-"$DATE"-"$DEVICE"-$MODEL\"/" $DIR/arch/arm64/configs/$KERNEL_DEFCONFIG
+
+    NAME=$(grep -o 'CONFIG_LOCALVERSION="[^"]*"' $DIR/arch/arm64/configs/$KERNEL_DEFCONFIG | cut -d '"' -f 2)
+    NAME=${NAME:1}.zip
+
+    if [[ "$LOCAL" == "y" ]] || [[ "$RELEASE" == "y" ]]; then
+        cd $DIR/build/out/$MODEL/zip
+        zip -r ../"$NAME" .
+        rm -rf $DIR/build/out/$MODEL/zip
+        mv $DIR/build/out/$MODEL/"$NAME" $DIR/build/export/"$NAME"
+        cd $DIR/build/export
+    fi
+}
+
+clean ()
+{
+    echo "---------------------------------------------------------"
+    echo "-- Cleanup Build Files..."
+    echo "---------------------------------------------------------"
+
+    cd $DIR && rm -rf o* .w* build/AIK/s* build/AIK/ramdisk/f* && git restore arch/arm64/configs/$KERNEL_DEFCONFIG build/u*
+
+    if [[ "$CLEAN" == "y" ]]; then	
+        rm -rf K* toolc* && git clean -df && git reset --hard HEAD
+    fi
+}
+
+# Main Function
+rm -rf ./build.log
+(
+    START=`date +%s`
+
+    echo "---------------------------------------------------------"
+    echo "-- Preparing the Build Environment..."
+
+    detect_env
+
+    if test -d "$DIR/toolchain"; then
+        echo "---------------------------------------------------------"
+        echo "-- Toolchain Directory Found!"
+        echo "---------------------------------------------------------"
+    else
+        toolchain
+    fi
+
+    if [[ "$KSU" == "y" ]]; then
+        kernelsu
+    fi
+
+    kernel
+    dtb
+    ramdisk
+    build_zip
+
+    if [[ "$LOCAL" == "y" ]]; then
+        clean
+    fi
+
+    END=`date +%s`
+
+    let "ELAPSED=$END-$START"
+
+    echo "---------------------------------------------------------"
+    echo "-- Total Compile Time was $(($ELAPSED / 60)) Minutes and $(($ELAPSED % 60)) Seconds"
+    echo "---------------------------------------------------------"
+) 2>&1	| tee -a ./build.log
---
+++ build/AIK/repackimg.sh
@@ -0,0 +1,406 @@
+#!/bin/bash
+# AIK-Linux/repackimg: repack ramdisk and build image
+# osm0sis @ xda-developers
+
+abort() { echo "-- Error!"; }
+
+case $1 in
+  --help) echo "-- Usage: repackimg.sh [--local] [--original] [--origsize] [--level <0-9>] [--avbkey <name>] [--forceelf]"; exit 1;
+esac;
+
+case $(uname -s) in
+  Darwin|Macintosh)
+    plat="macos";
+    readlink() { perl -MCwd -e 'print Cwd::abs_path shift' "$2"; }
+  ;;
+  *) plat="linux";;
+esac;
+arch=$plat/`uname -m`;
+
+aik="${BASH_SOURCE:-$0}";
+aik="$(dirname "$(readlink -f "$aik")")";
+bin="$aik/bin";
+cur="$(readlink -f "$PWD")";
+
+case $plat in
+  macos)
+    cpio="env DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/cpio"";
+    statarg="-f %Su";
+    dd() { DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/dd" "$@"; }
+    lzop() { DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/lzop" "$@"; }
+    truncate() { DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/truncate" "$@"; }
+    xz() { DYLD_LIBRARY_PATH="$bin/$arch" "$bin/$arch/xz" "$@"; }
+
+    javaver=$(java -version 2>&1 | head -n1 | cut -d\" -f2);
+    javamaj=$(echo $javaver | cut -d. -f1);
+    javamin=$(echo $javaver | cut -d. -f2);
+    if [ "$javamaj" -lt 9 ] && [ "$javamaj" -eq 1 -a "$javamin" -lt 8 ]; then
+      java() { "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java" "$@"; }
+    fi;
+  ;;
+  linux)
+    cpio=cpio;
+    [ "$(cpio --version | head -n1 | rev | cut -d\  -f1 | rev)" = "2.13" ] && cpiowarning=1;
+    statarg="-c %U";
+  ;;
+esac;
+
+case $1 in
+  --local) shift;;
+  *) cd "$aik";;
+esac;
+chmod -R 755 "$bin" "$aik/"*.sh;
+chmod 644 "$bin/magic" "$bin/androidbootimg.magic" "$bin/boot_signer.jar" "$bin/avb/"* "$bin/chromeos/"*;
+
+if [ -z "$(ls split_img/* 2>/dev/null)" -o ! -e ramdisk ]; then
+  echo "-- No Files Found to be Packed/Built.";
+  abort;
+  exit 1;
+fi;
+
+while [ "$1" ]; do
+  case $1 in
+    --original) original=1;;
+    --origsize) origsize=1;;
+    --forceelf) repackelf=1;;
+    --level)
+      case $2 in
+        ''|*[!0-9]*) ;;
+        *) level="-$2"; lvltxt=" - Level: $2"; shift;;
+      esac;
+    ;;
+    --avbkey)
+      if [ "$2" ]; then
+        for keytest in "$2" "$cur/$2" "$aik/$2"; do
+          if [ ! -z "$(ls $keytest.pk8 2>/dev/null)" -a ! -z "$(ls $keytest.x509.* 2>/dev/null)" ]; then
+            avbkey="$keytest"; avbtxt=" - Key: $2"; shift; break;
+          fi;
+        done;
+      fi;
+    ;;
+  esac;
+  shift;
+done;
+
+ramdiskcomp=`cat split_img/*-*ramdiskcomp 2>/dev/null`;
+if [ -z "$(ls ramdisk/* 2>/dev/null)" ] && [ ! "$ramdiskcomp" = "empty" -a ! "$original" ]; then
+  echo "-- No Files Found to be Packed/Built.";
+  abort;
+  exit 1;
+fi;
+
+echo "-- Android Image Kitchen - Repack Image Script";
+echo "-- By osm0sis @ xda-developers";
+echo "-- Edited by oItsMineZ @ github";
+echo "---------------------------------------------------------"
+
+if [ ! -z "$(ls *-new.* 2>/dev/null)" ]; then
+  echo "-- Warning: Overwriting Existing Files!";
+fi;
+rm -f "*-new.*";
+
+if [ -d ramdisk ] && [ "$(stat $statarg ramdisk | head -n 1)" = "root" ]; then
+  sudo=sudo; sumsg=" (as root)";
+else
+  cpioarg="-R 0:0";
+fi;
+
+if [ "$original" ]; then
+  echo "-- Repacking with original ramdisk...";
+elif [ "$ramdiskcomp" = "empty" ]; then
+  echo "-- Warning: Using empty ramdisk for repack!";
+  compext=.empty;
+  touch ramdisk-new.cpio$compext;
+else
+  echo "-- Packing ramdisk$sumsg...";
+  if [ -z "$level" ]; then
+    case $ramdiskcomp in
+      xz) level=-1;;
+      lz4*) level=-9;;
+    esac;
+  fi;
+  echo "-- Using compression: $ramdiskcomp$lvltxt";
+  repackcmd="$ramdiskcomp $level";
+  compext=$ramdiskcomp;
+  case $ramdiskcomp in
+    gzip) compext=gz;;
+    lzop) compext=lzo;;
+    xz) repackcmd="xz $level -Ccrc32";;
+    lzma) repackcmd="xz $level -Flzma";;
+    bzip2) compext=bz2;;
+    lz4) repackcmd="$bin/$arch/lz4 $level";;
+    lz4-l) repackcmd="$bin/$arch/lz4 $level -l"; compext=lz4;;
+    cpio) repackcmd="cat"; compext="";;
+    *) abort; exit 1;;
+  esac;
+  if [ "$compext" ]; then
+    compext=.$compext;
+  fi;
+  cd ramdisk;
+  $sudo find . | $sudo $cpio $cpioarg -H newc -o 2>/dev/null | $repackcmd > ../ramdisk-new.cpio$compext;
+  if [ ! $? -eq "0" ]; then
+    abort;
+    exit 1;
+  fi;
+  cd ..;
+fi;
+
+echo "---------------------------------------------------------"
+echo "-- Getting Build Information...";
+echo "---------------------------------------------------------"
+cd split_img;
+imgtype=`cat *-imgtype`;
+case $imgtype in
+  KRNL) ;;
+  AOSP_VNDR) vendor=vendor_;;
+  *)
+    if [ -f *-kernel ]; then
+      kernel=`ls *-kernel`;              echo "-- Kernel: $kernel";
+      kernel="split_img/$kernel";
+    fi;
+  ;;
+esac;
+if [ "$original" ]; then
+  ramdisk=`ls *-*ramdisk.cpio*`;         echo "-- ${vendor}Ramdisk: $ramdisk";
+  ramdisk="split_img/$ramdisk";
+else
+  ramdisk="ramdisk-new.cpio$compext";
+fi;
+case $imgtype in
+  KRNL) rsz=$(wc -c < ../"$ramdisk");    echo "-- Ramdisk Size: $rsz";;
+  OSIP)                                  echo "-- CMDline: $(cat *-*cmdline)";;
+  U-Boot)
+    name=`cat *-name`;                   echo "Name: $name";
+    uarch=`cat *-arch`;
+    os=`cat *-os`;
+    type=`cat *-type`;
+    comp=`cat *-comp`;                   echo "-- Type: $uarch $os $type ($comp)";
+    [ "$comp" = "uncompressed" ] && comp=none;
+    addr=`cat *-addr`;                   echo "-- Load ADDR: $addr";
+    ep=`cat *-ep`;                       echo "-- Entry Point: $ep";
+  ;;
+  *)
+    if [ -f *-second ]; then
+      second=`ls *-second`;              echo "-- Second: $second";
+      second=(--second "split_img/$second");
+    fi;
+    if [ -f *-dtb ]; then
+      dtb=`ls *-dtb`;                    echo "-- DTB: $dtb";
+      dtb=(--dtb "split_img/$dtb");
+    fi;
+    if [ -f *-recovery_dtbo ]; then
+      recoverydtbo=`ls *-recovery_dtbo`; echo "-- Recovery DTBO: $recoverydtbo";
+      recoverydtbo=(--recovery_dtbo "split_img/$recoverydtbo");
+    fi;
+    if [ -f *-*cmdline ]; then
+      cmdname=`ls *-*cmdline`;
+      cmdline=`cat *-*cmdline`;          echo "-- ${vendor}CMDline: $cmdline";
+      cmd=("split_img/$cmdname"@cmdline);
+    fi;
+    if [ -f *-board ]; then
+      board=`cat *-board`;               echo "-- Board: $board";
+    fi;
+    if [ -f *-base ]; then
+      base=`cat *-base`;                 echo "-- Base: $base";
+    fi;
+    if [ -f *-pagesize ]; then
+      pagesize=`cat *-pagesize`;         echo "--Pagesize: $pagesize";
+    fi;
+    if [ -f *-kernel_offset ]; then
+      kerneloff=`cat *-kernel_offset`;   echo "-- Kernel Offset: $kerneloff";
+    fi;
+    if [ -f *-ramdisk_offset ]; then
+      ramdiskoff=`cat *-ramdisk_offset`; echo "-- Ramdisk Offset: $ramdiskoff";
+    fi;
+    if [ -f *-second_offset ]; then
+      secondoff=`cat *-second_offset`;   echo "-- Second Offset: $secondoff";
+    fi;
+    if [ -f *-tags_offset ]; then
+      tagsoff=`cat *-tags_offset`;       echo "-- Tags Offset: $tagsoff";
+    fi;
+    if [ -f *-dtb_offset ]; then
+      dtboff=`cat *-dtb_offset`;         echo "-- DTB Offset: $dtboff";
+    fi;
+    if [ -f *-os_version ]; then
+      osver=`cat *-os_version`;          echo "-- OS Version: $osver";
+    fi;
+    if [ -f *-os_patch_level ]; then
+      oslvl=`cat *-os_patch_level`;      echo "-- OS Patch Level: $oslvl";
+    fi;
+    if [ -f *-header_version ]; then
+      hdrver=`cat *-header_version`;     echo "-- Header Version: $hdrver";
+    fi;
+    if [ -f *-hashtype ]; then
+      hashtype=`cat *-hashtype`;         echo "-- Hashtype: $hashtype";
+      hashtype="--hashtype $hashtype";
+    fi;
+    if [ -f *-dt ]; then
+      dttype=`cat *-dttype`;
+      dt=`ls *-dt`;                      echo "-- DT: $dt";
+      rpm=("split_img/$dt",rpm);
+      dt=(--dt "split_img/$dt");
+    fi;
+    if [ -f *-unknown ]; then
+      unknown=`cat *-unknown`;           echo "-- Unknown = $unknown";
+    fi;
+    if [ -f *-header ]; then
+      header=`ls *-header`;
+      header="split_img/$header";
+    fi;
+  ;;
+esac;
+cd ..;
+
+if [ -f split_img/*-mtktype ]; then
+  mtktype=`cat split_img/*-mtktype`;
+  echo " ";
+  echo "Generating MTK headers...";
+  echo " ";
+  echo "Using ramdisk type: $mtktype";
+  "$bin/$arch/mkmtkhdr" --kernel "$kernel" --$mtktype "$ramdisk" >/dev/null;
+  if [ ! $? -eq "0" ]; then
+    abort;
+    exit 1;
+  fi;
+  mv -f "$(basename "$kernel")-mtk" kernel-new.mtk;
+  mv -f "$(basename "$ramdisk")-mtk" $mtktype-new.mtk;
+  kernel=kernel-new.mtk;
+  ramdisk=$mtktype-new.mtk;
+fi;
+
+if [ -f split_img/*-sigtype ]; then
+  outname=unsigned-new.img;
+else
+  outname=image-new.img;
+fi;
+
+[ "$dttype" = "ELF" ] && repackelf=1;
+if [ "$imgtype" = "ELF" ] && [ ! "$header" -o ! "$repackelf" ]; then
+  imgtype=AOSP;
+  echo "---------------------------------------------------------"
+  echo "-- Warning: ELF Format Without RPM Detected; will be Repacked using AOSP Format!";
+fi;
+
+echo "---------------------------------------------------------"
+echo "Building Kernel Image...";
+echo "---------------------------------------------------------"
+echo "Using Format: $imgtype";
+echo " ";
+case $imgtype in
+  AOSP_VNDR) "$bin/$arch/mkbootimg" --vendor_ramdisk "$ramdisk" "${dtb[@]}" --vendor_cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --tags_offset $tagsoff --dtb_offset $dtboff --os_version "$osver" --os_patch_level "$oslvl" --header_version $hdrver --vendor_boot $outname;;
+  AOSP) "$bin/$arch/mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" "${dtb[@]}" "${recoverydtbo[@]}" --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --second_offset "$secondoff" --tags_offset "$tagsoff" --dtb_offset "$dtboff" --os_version "$osver" --os_patch_level "$oslvl" --header_version "$hdrver" $hashtype "${dt[@]}" -o $outname;;
+  AOSP-PXA) "$bin/$arch/pxa-mkbootimg" --kernel "$kernel" --ramdisk "$ramdisk" "${second[@]}" --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --second_offset "$secondoff" --tags_offset "$tagsoff" --unknown $unknown "${dt[@]}" -o $outname;;
+  ELF) "$bin/$arch/elftool" pack -o $outname header="$header" "$kernel" "$ramdisk",ramdisk "${rpm[@]}" "${cmd[@]}" >/dev/null;;
+  KRNL) "$bin/$arch/rkcrc" -k "$ramdisk" $outname;;
+  OSIP)
+    mkdir split_img/.temp 2>/dev/null;
+    for i in bootstub cmdline.txt hdr kernel parameter sig; do
+      cp -f split_img/*-*$(basename $i .txt | sed -e 's/hdr/header/') split_img/.temp/$i 2>/dev/null;
+    done;
+    cp -f "$ramdisk" split_img/.temp/ramdisk.cpio.gz;
+    "$bin/$arch/mboot" -d split_img/.temp -f $outname;
+  ;;
+  U-Boot)
+    part0="$kernel";
+    case $type in
+      Multi) part1=(:"$ramdisk");;
+      RAMDisk) part0="$ramdisk";;
+    esac;
+    "$bin/$arch/mkimage" -A $uarch -O $os -T $type -C $comp -a $addr -e $ep -n "$name" -d "$part0""${part1[@]}" $outname >/dev/null;
+  ;;
+  *) echo " "; echo "Unsupported format."; abort; exit 1;;
+esac;
+if [ ! $? -eq "0" ]; then
+  abort;
+  exit 1;
+fi;
+
+rm -rf split_img/.temp;
+
+if [ -f split_img/*-sigtype ]; then
+  sigtype=`cat split_img/*-sigtype`;
+  if [ -f split_img/*-avbtype ]; then
+    avbtype=`cat split_img/*-avbtype`;
+  fi;
+  if [ -f split_img/*-blobtype ]; then
+    blobtype=`cat split_img/*-blobtype`;
+  fi;
+  echo "-- Signing New Image...";
+  echo "---------------------------------------------------------"
+  echo "-- Using Signature: $sigtype $avbtype$avbtxt$blobtype";
+  [ ! "$avbkey" ] && avbkey="$bin/avb/verity";
+  echo "---------------------------------------------------------"
+  case $sigtype in
+    AVBv1) java -jar "$bin/boot_signer.jar" /$avbtype unsigned-new.img "$avbkey.pk8" "$avbkey.x509."* image-new.img 2>/dev/null;;
+    BLOB)
+      awk 'BEGIN { printf "-SIGNED-BY-SIGNBLOB-\00\00\00\00\00\00\00\00" }' > image-new.img;
+      "$bin/$arch/blobpack" blob.tmp $blobtype unsigned-new.img >/dev/null;
+      cat blob.tmp >> image-new.img;
+      rm -f blob.tmp;
+    ;;
+    CHROMEOS) "$bin/$arch/futility" vbutil_kernel --pack image-new.img --keyblock "$bin/chromeos/kernel.keyblock" --signprivate "$bin/chromeos/kernel_data_key.vbprivk" --version 1 --vmlinuz unsigned-new.img --bootloader "$bin/chromeos/empty" --config "$bin/chromeos/empty" --arch arm --flags 0x1;;
+    DHTB)
+      "$bin/$arch/dhtbsign" -i unsigned-new.img -o image-new.img >/dev/null;
+      rm -f split_img/*-tailtype 2>/dev/null;
+    ;;
+    NOOK*) cat split_img/*-master_boot.key unsigned-new.img > image-new.img;;
+  esac;
+  if [ ! $? -eq "0" ]; then
+    abort;
+    exit 1;
+  fi;
+fi;
+
+if [ -f split_img/*-lokitype ]; then
+  lokitype=`cat split_img/*-lokitype`;
+  echo "-- Loki Patching New Image...";
+  echo "---------------------------------------------------------"
+  echo "-- Using Type: $lokitype";
+  echo "---------------------------------------------------------"
+  mv -f image-new.img unlokied-new.img;
+  if [ -f aboot.img ]; then
+    "$bin/$arch/loki_tool" patch $lokitype aboot.img unlokied-new.img image-new.img >/dev/null;
+    if [ ! $? -eq "0" ]; then
+      echo "-- Patching Failed!";
+      abort;
+      exit 1;
+    fi;
+  else
+    echo "-- Device aboot.img Required in Script Directory to Find Loki Patch Offset.";
+    abort;
+    exit 1;
+  fi;
+elif [ -f split_img/*-microloader.bin ]; then
+  echo "-- Amonet Patching New Image...";
+  echo "---------------------------------------------------------"
+  cp -f image-new.img unamonet-new.img;
+  cp -f split_img/*-microloader.bin microloader.tmp;
+  dd bs=1024 count=1 conv=notrunc if=unamonet-new.img of=head.tmp 2>/dev/null;
+  dd bs=1024 seek=1 conv=notrunc if=head.tmp of=image-new.img 2>/dev/null;
+  dd conv=notrunc if=microloader.tmp of=image-new.img 2>/dev/null;
+  rm -f head.tmp microloader.tmp;
+fi;
+
+if [ -f split_img/*-tailtype ]; then
+  tailtype=`cat split_img/*-tailtype`;
+  echo "-- Appending Footer...";
+  echo "---------------------------------------------------------"
+  echo "-- Using Type: $tailtype";
+  echo "---------------------------------------------------------"
+  case $tailtype in
+    Bump) awk 'BEGIN { printf "\x41\xA9\xE4\x67\x74\x4D\x1D\x1B\xA4\x29\xF2\xEC\xEA\x65\x52\x79" }' >> image-new.img;;
+    SEAndroid) printf 'SEANDROIDENFORCE' >> image-new.img;;
+  esac;
+fi;
+
+if [ "$origsize" -a -f split_img/*-origsize ]; then
+  filesize=`cat split_img/*-origsize`;
+  echo "-- Padding to Original Size...";
+  echo "---------------------------------------------------------"
+  cp -f image-new.img unpadded-new.img;
+  truncate -s $filesize image-new.img;
+fi;
+
+echo "-- Done!";
+exit 0;
+
