9fc9a62cfce36340244c38e77612ed579ab77110
oItsMineZ <135024283+oItsMineZ@users.noreply.github.com>
2025-05-08 18:06:00 -0700

    build: use Clang 16.0.2 (Based on r475365b)

--- build.sh
+++ build.sh
@@ -166,9 +166,9 @@ toolchain ()
     echo "-- Checkout Toolchain Repo..."
     echo "---------------------------------------------------------"
 
-    git submodule add -f https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r468909b.git toolchain/clang-r468909b
+    git submodule add -f https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r475365b.git toolchain/clang-r475365b
 
-    CLANG=$DIR/toolchain/clang-r468909b
+    CLANG=$DIR/toolchain/clang-r475365b
     PATH=$CLANG/bin:$CLANG/lib:$PATH
     ARGS="
         ARCH=arm64 O=out \
